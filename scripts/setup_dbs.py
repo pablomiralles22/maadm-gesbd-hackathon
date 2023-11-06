e1f2246aa29e3852249b95d18891114bf391aabe
alvaro <alvarorgzalonso@gmail.com>
2023-11-06 20:21:13 +0100

    creado elasticsearch notebook y añadido elasticsearch en setup_dbs

--- scripts/setup_dbs.py
+++ scripts/setup_dbs.py
@@ -1,4 +1,5 @@
 import pymongo
+from elasticsearch import Elasticsearch
 import sys
 
 from dotenv import dotenv_values
@@ -13,19 +14,69 @@ env_config = dotenv_values(env_path)
 ##### MONGODB #####
 print('Setting up MongoDB...')
 
-client = pymongo.MongoClient(
+client_mongo = pymongo.MongoClient(
     host=env_config['MONGODB_HOST'],
     port=int(env_config['MONGODB_PORT']),
     username=env_config['MONGO_USER'],
     password=env_config['MONGO_PASSWORD'],
 )
-db = client["boe_db"]
+db = client_mongo["boe_db"]
 collection = db["boe"]
 
 collection.create_index([("identificador", pymongo.DESCENDING)], name="id_index", unique=True)
 print('Done!')
 
 ##### ELASTICSEARCH #####
+print('Setting up Elasticsearch...')
+client = Elasticsearch(f"http://{env_config['ELASTICSEARCH_HOST']}:{env_config['ELASTICSEARCH_PORT']}")
 
+es_config = {
+    "mappings": {
+        "properties": {
+            "texto": {
+                "type": "text"
+            },
+            "semantic_embeddig": {
+                "type": "dense_vector",
+                "dims": 512,
+                "index": True,
+                "similarity": "cosine"
+            },
+            #"titulo": {"type": "text"},
+            "doc_id": {"type": "text"},
+            #"fecha_publicacion": {"type": "date"}
+        }
+    },
+    "settings": {
+        "analysis": {
+            "analyzer": {
+                "my_analyzer": {
+                "tokenizer": "keyword",
+                "char_filter": [
+                    "html_strip"
+                ]
+                }
+            }
+        },
+        "number_of_shards": 2,
+        "number_of_replicas": 1
+    }
+}
 
+my_index = "boe"
+try:
+    client.indices.delete(index=my_index)
+    print("Index deleted. Recreating index...")
+except:
+    print("Index does not exist. Creating...")
+try:
+    client.indices.create(
+        index = my_index,
+        settings = es_config["settings"],
+        mappings = es_config["mappings"]
+    )
+except Exception as error:
+    print("Error:", error)
+finally:
+    print("Done!")
 ##### GRAPHDB #####
